81d70d32eddc75c2df8c5d159ec85907aff0b4a8
Greg Baker <gba311@gmail.com>
2013-04-15 18:35:29 +0200

    more refactoring of time_formatter

--- lib/photo/time_formatter.rb
+++ lib/photo/time_formatter.rb
@@ -1,31 +1,33 @@
 module Photo
   module TimeFormatter
 
-    def to_time_sentence
-      sentence_from float_time
+    def to_time_sentence 
+      deliminate sentence_parts(float_to_time)
     end
 
     private
 
-    def float_time(seconds=self.to_i)
+    def deliminate(parts)
+      case parts.size
+      when 0 then "#{self.round(4)} seconds" 
+      when 1 then "#{parts[0]}" 
+      when 2 then parts.join(' and ')
+      else 
+        "#{parts[0]}, #{parts[1]} and #{parts[2]}"
+      end
+    end
+
+    def sentence_parts(time)
+      time.map { |unit, value| plurality(unit, value) }.compact
+    end
+
+    def float_to_time(seconds=self.to_i)
       hours = seconds / 3600
       seconds -= (3600 * hours)
       minutes = seconds / 60
       seconds -= (60 * minutes)
       {hour: hours, minute: minutes, second: seconds}
     end 
-
-    def sentence_from(time)
-      parts = sentence_parts(time)
-      return "#{self.round(4)} seconds" if parts.none?
-      return "#{parts[0]}" if parts.size == 1
-      return parts.join(' and ') if parts.size == 2
-      "#{parts[0]}, #{parts[1]} and #{parts[2]}"
-    end
-
-    def sentence_parts(time)
-      time.map { |unit, value| plurality(unit, value) }.compact
-    end
 
     def plurality(key, value)
       return if value == 0
