fcb2c0f09efecc25bae27f5300ec1d843638f603
Greg Baker <gba311@gmail.com>
2013-06-14 11:42:30 +0200

    reorder

--- lib/photo/fetch.rb
+++ lib/photo/fetch.rb
@@ -22,19 +22,6 @@ module Photo
       end
     end
 
-    def fetch_files media, media_type
-      if media.any?
-      new_files = filter_duplicated media, media_type
-        if new_files.empty?
-          notify_files_up_to_date media_type
-        else
-          copy_files new_files, media_type
-        end
-      else
-        notify_files_not_found media_type
-      end
-    end
-
     def source_photos
       photos = source_files_with_ext @settings[:photo_ext]
       fetch_files(photos, 'Photos')
@@ -49,6 +36,18 @@ module Photo
       Dir.glob("#{@settings[:source]}/**/*.#{extension}")
     end
 
+    def fetch_files media, media_type
+      if media.any?
+        new_files = filter_duplicated media, media_type
+        if new_files.empty?
+          notify_files_up_to_date media_type
+        else
+          copy_files new_files, media_type
+        end
+      else
+        notify_files_not_found media_type
+      end
+    end
 
     def filter_duplicated media, media_type
       target_paths = {}
@@ -66,14 +65,6 @@ module Photo
         progress.increment }
     end
 
-    def notify_files_up_to_date media_type
-      @output.puts " #{media_type} are up-to-date".color(:green)
-    end
-
-    def notify_files_not_found media_type
-      @output.puts " No #{media_type.downcase} found"
-    end
-
     def fetch_file(file, destination)
       dirname = File.dirname(destination)
       FileUtils.mkdir_p(dirname) unless File.exists?(dirname)
@@ -83,6 +74,14 @@ module Photo
     def folder_name_for file, media_type
       ctime = File.open(file) { |f| Date.parse(f.ctime.to_s) }
       "#{ctime.year.to_s}/#{ctime.strftime('%m %B')}/#{ctime.strftime('%e')}/#{media_type}"
+    end
+
+    def notify_files_up_to_date media_type
+      @output.puts " #{media_type} are up-to-date".color(:green)
+    end
+
+    def notify_files_not_found media_type
+      @output.puts " No #{media_type.downcase} found"
     end
   end
 end
